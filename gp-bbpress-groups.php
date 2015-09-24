8e0cfe12eea9388e650ddc429c3532e7b11abbed
petercorlettwiley <peter.c.wiley@gmail.com>
2015-09-24 17:25:59 -0500

    Extended capability-adding function to bbPress replies (still a bit kluge-y)

--- gp-bbpress-groups.php
+++ gp-bbpress-groups.php
@@ -1,21 +1,28 @@
 <?php
 /*
 Plugin Name: GasPedal bbPress + Groups Integration
-Description: Addon to auto-assign Groups read capabilities to new bbPress topics and replies
+Description: Addon to auto-assign Groups read capabilities to new bbPress threads
 Author: Peter Wiley
 Version: 0.0.1
 */
 
 function gpbbp_new_topic() {
-
   $forum_capabilities = Groups_Post_access::get_read_post_capabilities( bbp_get_forum_id() );
   foreach( $forum_capabilities as $capability ) {
     Groups_Post_Access::create( array( 'post_id' => bbp_get_topic_id(), 'capability' => $capability ));
-    echo $capability;
   }
   unset( $capability );
 }
 add_action( 'bbp_theme_before_topic_title', 'gpbbp_new_topic' );
+
+function gpbbp_new_reply() {
+  $forum_capabilities = Groups_Post_access::get_read_post_capabilities( bbp_get_forum_id() );
+  foreach( $forum_capabilities as $capability ) {
+    Groups_Post_Access::create( array( 'post_id' => bbp_get_reply_id(), 'capability' => $capability ));
+  }
+  unset( $capability );  
+}
+add_action( 'bbp_template_before_single_reply', 'gpbbp_new_reply' );
 
 
 // 
